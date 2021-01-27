f6d924be70601c8febe0f045a23a7a2b535db6c9
SarahMussen <sarah.mussen@student.howest.be>
2021-01-27 23:33:23 +0100

    changed the condition to unlock an item; added the new design

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class GameManager : MonoBehaviour
 {
@@ -16,56 +17,33 @@ public class GameManager : MonoBehaviour
     private bool winner = false;
     private bool gameEnded = false;
 
-    [SerializeField]
-    private TextMeshProUGUI catchedPoopText;
+    private int itemsUnlocked = 0;
+
+
+    //TEST 
     [SerializeField]
     private TextMeshProUGUI missedPoopText;
 
+    //downgrade canvas
+    [SerializeField]
+    private Slider downgradeSlider;
 
+    //upgrade canvas
     [SerializeField]
     private Slider upgradeSlider;
     [SerializeField]
-    private Slider downgradeSlider;
-    [SerializeField]
     private TextMeshProUGUI upgradeText;
-    [SerializeField]
-    private TextMeshProUGUI downgradeText;
-
 
     [SerializeField]
-    private RawImage firstItemImg;
+    private RawImage[] itemIcons;
     [SerializeField]
-    private Texture firstItemTexture;
+    private Texture[] itemTextures;
     [SerializeField]
-    private RawImage secondItemImg;
+    private string[] itemDescriptions;
     [SerializeField]
-    private Texture secondItemTexture;
-    [SerializeField]
-    private RawImage thirdItemImg;
-    [SerializeField]
-    private Texture thirdItemTexture;
-    [SerializeField]
-    private RawImage fourthItemImg;
-    [SerializeField]
-    private Texture fourthItemTexture;
+    private Texture[] popupImages;
 
-    [SerializeField]
-    private RawImage firstEnvironmentImg;
-    [SerializeField]
-    private Texture firstEnvironmentTexture;
-    [SerializeField]
-    private RawImage secondEnvironmentImg;
-    [SerializeField]
-    private Texture secondEnvironmentTexture;
-    [SerializeField]
-    private RawImage thirdEnvironmentImg;
-    [SerializeField]
-    private Texture thirdEnvironmentTexture;
-    [SerializeField]
-    private RawImage fourthEnvironmentImg;
-    [SerializeField]
-    private Texture fourthEnvironmentTexture;
-
+    //popup canvas
     [SerializeField]
     private Canvas popUpCanvas;
     [SerializeField]
@@ -77,6 +55,7 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private Button popUpButton;
 
+    //ending canvas
     [SerializeField]
     private Canvas endingCanvas;
     [SerializeField]
@@ -88,151 +67,82 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private Texture winnerImg;
 
+
     void Awake()
     {
         if (Instance == null) 
         { 
-            Instance = this; 
+            Instance = this;
         }
     }
 
-    /// <summary>
-    /// adds one to the catched variable when the player catches a poop
-    /// </summary>
     public void addCathedPoop()
     {
         catchedPoop++;
-        catchedPoopText.text = "score: " + catchedPoop;
-        updateUpgradeGUI();
 
-        if(catchedPoop == 20)
+        updateScoreGUI();
+
+        if (catchedPoop == 5)
         {
+            itemsUnlocked++;
+            openPopUp(itemsUnlocked);
+        }
+    }
+
+    public void addMissedPoop()
+    {
+        missedPoop++;
+        missedPoopText.text = "missed: " + missedPoop;  //TEST
+
+        if(missedPoop%5 == 0)
+        {
+            updateDowngradeGUI();
+        }
+    }
+
+    private void updateScoreGUI()
+    {
+        upgradeText.text = catchedPoop + "/5";
+    }
+
+    private void openPopUp(int itemToUnlock)
+    {
+        itemIcons[itemToUnlock-1].texture = itemTextures[itemToUnlock-1];
+        popUpImg.texture = popupImages[itemToUnlock-1];
+        popUpText.text = itemDescriptions[itemToUnlock-1];
+
+        popUpCanvas.gameObject.SetActive(true);
+
+        Time.timeScale = 0;
+
+        if (itemToUnlock == 4)
+        {
+            gameEnded = true;
             winner = true;
         }
     }
 
-
-    /// <summary>
-    /// adds one to the missedPoop variable when the player misses a poop
-    /// </summary>
-    public void addMissedPoop()
-    {
-        missedPoop++;
-        missedPoopText.text = "missed: " + missedPoop;
-        updateDowngradeGUI();
-    }
-
-    /// <summary>
-    /// Updates the upgrade gui depending on how much catched poop;
-    /// Changes the slider, the text and the image
-    /// </summary>
-    private void updateUpgradeGUI()
-    {
-        switch (catchedPoop)
-        {
-            case 5:
-                upgradeSlider.value = 25;
-                upgradeText.text = "1/4 items gevonden";
-                firstItemImg.texture = firstItemTexture;
-
-                openPopUp(firstItemTexture, "Je hebt genoeg mest verzameld om je allereerste item te maken, namelijk een t-shirt!");
-                break;
-            case 10:
-                upgradeSlider.value = 50;
-                upgradeText.text = "2/4 items gevonden";
-                secondItemImg.texture = secondItemTexture;
-
-                openPopUp(secondItemTexture, "Je hebt genoeg mest verzameld om een tweede item te maken, namelijk een cowpot!");
-
-                break;
-            case 15:
-                upgradeSlider.value = 75;
-                upgradeText.text = "3/4 items gevonden";
-                thirdItemImg.texture = thirdItemTexture;
-
-                openPopUp(thirdItemTexture, "Je hebt genoeg mest verzameld om een derde item te maken, namelijk een merdapot!");
-
-                break;
-            case 20:
-                upgradeSlider.value = 100;
-                upgradeText.text = "4/4 items gevonden";
-                fourthItemImg.texture = fourthItemTexture;
-                gameEnded = true;
-                openPopUp(fourthItemTexture, "Je hebt genoeg mest verzameld om een vierde item te maken, namelijk wc papier!");
-                break;
-            default:
-                break;
-        }
-    }
-
-    /// <summary>
-    /// Updates the downgrade gui depending on how much missed poop;
-    /// Changes the slider, the text and the image
-    /// </summary>
     private void updateDowngradeGUI()
     {
-        switch (missedPoop)
-        {
-            case 5:
-                downgradeSlider.value = 75;
-                downgradeText.text = "75% omgeving";
-                firstEnvironmentImg.texture = firstEnvironmentTexture;
-                break;
-            case 10:
-                downgradeSlider.value = 50;
-                downgradeText.text = "50% omgeving";
-                secondEnvironmentImg.texture = secondEnvironmentTexture;
-                break;
-            case 15:
-                downgradeSlider.value = 25;
-                downgradeText.text = "25% omgeving";
-                thirdEnvironmentImg.texture = thirdEnvironmentTexture;
-                break;
-            case 20:
-                downgradeSlider.value = 0;
-                downgradeText.text = "0% omgeving";
-                fourthEnvironmentImg.texture = fourthEnvironmentTexture;
-                gameEnded = true;
-                break;
-            default:
-                break;
-        }
+        downgradeSlider.value = downgradeSlider.value - 25;
+
     }
 
-    /// <summary>
-    /// A popup is shown with a certain image, title and text. When the popup opens, the game is paused
-    /// </summary>
-    /// <param name="img">The texture that needs to be shown in the popup</param>
-    /// <param name="detailText">The explanation of the unlocked item/environmental change</param>
-    private void openPopUp(Texture img, string detailText)
-    {
-        popUpImg.texture = img;
-        popUpText.text = detailText;
-        popUpCanvas.gameObject.SetActive(true);
-
-        Time.timeScale = 0;
-    }
-
-    /// <summary>
-    /// close popUp canvas and continues game. 
-    /// has to be publid so the button can access the function
-    /// </summary>
     public void closePopUp()
     {
-        popUpCanvas.gameObject.SetActive(false);
 
         if (gameEnded)
         {
             endGame();
-        } else
+        }
+        else
         {
+            catchedPoop = 0;
+            popUpCanvas.gameObject.SetActive(false);
             Time.timeScale = 1;
         }
     }
 
-    /// <summary>
-    /// changes the ending popup if the player is the winner, pauses the game en shows the ending popup
-    /// </summary>
     private void endGame()
     {
         if (winner)
@@ -245,9 +155,6 @@ public class GameManager : MonoBehaviour
         endingCanvas.gameObject.SetActive(true);
     }
 
-    /// <summary>
-    /// restarts the game
-    /// </summary>
     public void restartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
