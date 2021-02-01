4799e5989b332d6ca8f08877b357a782412b72fb
SarahMussen <sarah.mussen@student.howest.be>
2021-02-01 20:49:16 +0100

    add materials and code to change environment

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -79,11 +79,83 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private GameObject cowPot;
 
+    //environment
+    [SerializeField]
+    private GameObject tree1;
+    [SerializeField]
+    private GameObject tree2;
+    [SerializeField]
+    private GameObject tree3;
+    [SerializeField]
+    private GameObject tree4;
+    [SerializeField]
+    private GameObject tree5;
+    [SerializeField]
+    private GameObject tree6;
+    [SerializeField]
+    private Material applesBad;
+    [SerializeField]
+    private Material treeBad;
+    [SerializeField]
+    private Material treeTrunkBad;
+
+    [SerializeField]
+    private GameObject flowerRed1;
+    [SerializeField]
+    private GameObject flowerRed2;
+    [SerializeField]
+    private GameObject flowerPurple3;
+    [SerializeField]
+    private GameObject flowerPurple4;
+    [SerializeField]
+    private GameObject flowerPink5;
+    [SerializeField]
+    private GameObject flowerPink6;
+    [SerializeField]
+    private Material flowerCenterBad;
+    [SerializeField]
+    private Material flowerStemBad;
+    [SerializeField]
+    private Material flowerRedLeafBad;
+    [SerializeField]
+    private Material flowerPinkLeafBad;
+    [SerializeField]
+    private Material flowerPurpleLeafBad;
+
+    [SerializeField]
+    private GameObject plane;
+    [SerializeField]
+    private Material grassBad;
+    [SerializeField]
+    private Material waterBad;
+
+    [SerializeField]
+    private GameObject airFarmer;
+    [SerializeField]
+    private GameObject airCow;
+
     //renderers
     private Renderer rendBody;
     private Renderer rendLegLeft;
     private Renderer rendLegRight;
 
+    private Renderer rendAppleTree1;
+    private Renderer rendAppleTree2;
+    private Renderer rendTree3;
+    private Renderer rendTree4;
+    private Renderer rendTree5;
+    private Renderer rendTree6;
+
+    private Renderer rendRedFlower1;
+    private Renderer rendRedFlower2;
+    private Renderer rendPurpleFlower3;
+    private Renderer rendPurpleFlower4;
+    private Renderer rendPinkFlower5;
+    private Renderer rendPinkFlower6;
+
+    private Renderer rendPlane;
+/*    private Renderer rendAirFarmer;
+    private Renderer rendAirCow;*/
 
     void Awake()
     {
@@ -98,9 +170,25 @@ public class GameManager : MonoBehaviour
         rendBody = farmerBody.GetComponent<Renderer>();
         rendLegLeft = farmerLegLeft.GetComponent<Renderer>();
         rendLegRight = farmerLegRight.GetComponent<Renderer>();
-        rendBody.enabled = true;
-        rendLegLeft.enabled = true;
-        rendLegRight.enabled = true;
+
+        rendAppleTree1 = tree1.GetComponent<Renderer>();
+        rendAppleTree2 = tree2.GetComponent<Renderer>();
+        rendTree3 = tree3.GetComponent<Renderer>();
+        rendTree4 = tree4.GetComponent<Renderer>();
+        rendTree5 = tree5.GetComponent<Renderer>();
+        rendTree6 = tree6.GetComponent<Renderer>();
+
+        rendRedFlower1 = flowerRed1.GetComponent<Renderer>();
+        rendRedFlower2 = flowerRed2.GetComponent<Renderer>();
+        rendPurpleFlower3 = flowerPurple3.GetComponent<Renderer>();
+        rendPurpleFlower4 = flowerPurple4.GetComponent<Renderer>();
+        rendPinkFlower5 = flowerPink5.GetComponent<Renderer>();
+        rendPinkFlower6 = flowerPink6.GetComponent<Renderer>();
+
+        rendPlane = plane.GetComponent<Renderer>();
+
+/*        rendAirFarmer = airFarmer.GetComponent<Renderer>();
+        rendAirCow = rendAirCow.GetComponent<Renderer>();*/
     }
 
     public int getCatchedPoop()
@@ -113,11 +201,6 @@ public class GameManager : MonoBehaviour
         return missedPoop;
     }
 
-    /// <summary>
-    /// increases the catchedPoop score with 1
-    /// calls the updateScoreGUI function
-    /// if the catchedPoop score equals 5, it increases the itemsUnlocked value en calls the openPopUp function
-    /// </summary>
     public void addCathedPoop()
     {
         catchedPoop++;
@@ -159,6 +242,7 @@ public class GameManager : MonoBehaviour
 
         if (itemToUnlock == 1)
         {
+
             var matsBody = rendBody.materials;
             matsBody[1] = blue;
             rendBody.materials = matsBody;
@@ -193,6 +277,71 @@ public class GameManager : MonoBehaviour
     private void updateDowngradeGUI()
     {
         downgradeSlider.value = downgradeSlider.value - 1;
+
+        switch (downgradeSlider.value)
+        {
+            //bloemen en gras
+            case 3:
+                var matsRedFlower = rendRedFlower1.materials;
+                var matsPinkFlower = rendPinkFlower5.materials;
+                var matsPurpleFlower = rendPurpleFlower3.materials;
+
+                matsRedFlower[0] = flowerRedLeafBad;
+                matsRedFlower[1] = flowerCenterBad;
+                matsRedFlower[2] = flowerStemBad;
+
+                rendRedFlower1.materials = matsRedFlower;
+                rendRedFlower2.materials = matsRedFlower;
+
+                matsPurpleFlower[0] = flowerPurpleLeafBad;
+                matsPurpleFlower[1] = flowerCenterBad;
+                matsPurpleFlower[2] = flowerStemBad;
+
+                rendPurpleFlower3.materials = matsPurpleFlower;
+                rendPurpleFlower4.materials = matsPurpleFlower;
+
+                matsPinkFlower[0] = flowerPinkLeafBad;
+                matsPinkFlower[1] = flowerCenterBad;
+                matsPinkFlower[2] = flowerStemBad;
+
+                rendPinkFlower5.materials = matsPinkFlower;
+                rendPinkFlower6.materials = matsPinkFlower;
+
+                var matsPlane = rendPlane.materials;
+                matsPlane[1] = grassBad;
+                rendPlane.materials = matsPlane;
+                break;
+
+            //bomen
+            case 2:
+                var matsAppleTree = rendAppleTree1.materials;
+                matsAppleTree[0] = applesBad;
+                matsAppleTree[1] = treeBad;
+                matsAppleTree[2] = treeTrunkBad;
+                rendAppleTree1.materials = matsAppleTree;
+                rendAppleTree2.materials = matsAppleTree;
+
+                var matsTree = rendTree3.materials;
+                matsTree[0] = treeBad;
+                matsTree[1] = treeTrunkBad;
+                rendTree3.materials = matsTree;
+                rendTree4.materials = matsTree;
+                rendTree5.materials = matsTree;
+                rendTree6.materials = matsTree;
+                break;
+
+            //lucht en water
+            case 1:
+
+
+                matsPlane = rendPlane.materials;
+                matsPlane[3] = waterBad;
+                rendPlane.materials = matsPlane;
+
+                break;
+            default:
+                break;
+        }
 
         if(downgradeSlider.value == 0)
         {
